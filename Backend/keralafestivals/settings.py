937c71bc909410f8704444b849a5aee44460a8bc
Godwin vinson <godwinvinson69@gmail.com>
2023-05-13 20:21:56 +0530

    new

--- Backend/keralafestivals/settings.py
+++ Backend/keralafestivals/settings.py
@@ -186,3 +186,15 @@ import dj_database_url
 DATABASES={
     'default':dj_database_url.parse(env('DATABASE_URL'))
 }
+
+
+# <-------------AWs s3 bucket---------->
+AWS_ACCESS_KEY_ID = 'AKIAWE22UGGTK4THVP33'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'AWS_STORAGE_BUCKET_NAME'
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME = 'AWS_S3_REGION_NAME'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL =  None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
