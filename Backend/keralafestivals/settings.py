f266819cefd7a6f8b4338faa74f58d96f61cfc63
Godwin vinson <godwinvinson69@gmail.com>
2023-05-14 12:43:19 +0530

    new

--- Backend/keralafestivals/settings.py
+++ Backend/keralafestivals/settings.py
@@ -191,7 +191,7 @@ DATABASES={
 # <-------------AWs s3 bucket---------->
 AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'kerala-festivalsbackend'
+AWS_STORAGE_BUCKET_NAME = 'keralavisionbackend'
 AWS_S3_SIGNATURE_NAME = 's3v4',
 AWS_S3_REGION_NAME = 'ap-south-1'
 AWS_S3_FILE_OVERWRITE = False
