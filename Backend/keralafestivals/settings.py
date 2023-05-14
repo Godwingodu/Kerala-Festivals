fac2fe306e613a78a525ae57d1bd45711ba1d685
Godwin vinson <godwinvinson69@gmail.com>
2023-05-14 09:44:24 +0530

    new

--- Backend/keralafestivals/settings.py
+++ Backend/keralafestivals/settings.py
@@ -161,7 +161,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "godwinvinson69@gmail.com"
-EMAIL_HOST_PASSWORD = env('EMAILPASSWORD')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 DEFAULT_FROMEMAIL='Celery <godwinvinson69@gmail.com>'
 
 
