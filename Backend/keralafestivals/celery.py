7179eb7169a46f01e861e253b565012c281f301e
Godwin vinson <godwinvinson69@gmail.com>
2023-05-17 02:37:34 +0530

    new

--- Backend/keralafestivals/celery.py
+++ Backend/keralafestivals/celery.py
@@ -18,7 +18,7 @@ def debug_task(self):
 app.conf.beat_schedule={
     'send-mail-every-day-at-8':{
         'task':'app.task.send_mail_func',
-        'schedule':crontab(hour=2,minute=1),
+        'schedule':crontab(hour=2,minute=41),
     }
 }
 
