0370064f80f95152ab5c05c22845d0abadc2e31f
Godwin vinson <godwinvinson69@gmail.com>
2023-05-18 02:29:44 +0530

    new

--- Backend/app/admin.py
+++ Backend/app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Events
+from .models import *
 # Register your models here.
 
 
 admin.site.register(Events)
+admin.site.register(subscribedusers)
+admin.site.register(booking)
