5b98e5b8657a9f504dc26ffb712d6e001f68a157
Godwin vinson <godwinvinson69@gmail.com>
2023-05-22 15:33:42 +0530

    new

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit():
   void {
-    // this.checkAuthStatus();
+    this.checkAuthStatus();
 
     // setTimeout(() => {
     //   if (localStorage.getItem("token")) {
