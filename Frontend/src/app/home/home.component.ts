56493de2aec97309823cb2bc6eec4f1cd61da4c6
Godwin vinson <godwinvinson69@gmail.com>
2023-05-24 20:27:48 +0530

    new

--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -10,6 +10,15 @@ import { Component} from '@angular/core';
 export class HomeComponent {
   constructor()
   {
-    
+    var nave = localStorage.getItem("register")
+    if (nave != "navbar") {
+      location.reload()
+    }
+    localStorage.setItem('register','navbar')
+
+    setTimeout(() => {
+      localStorage.removeItem('register')
+    }, 300);
+
   }
 }
